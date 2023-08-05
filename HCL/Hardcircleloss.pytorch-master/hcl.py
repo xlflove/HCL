be04251dc808ae8e6623b98b5f6f9913702b6795
xlflove <1109858988@qq.com>
2023-08-05 17:06:59 +0800

    Add files via upload

---
+++ HCL/Hardcircleloss.pytorch-master/hcl.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+import torch
+from torch import nn, Tensor
+
+
+def convert_label_to_similarity(normed_feature: Tensor, label: Tensor) -> Tuple[Tensor, Tensor]:
+    similarity_matrix = normed_feature @ normed_feature.transpose(1, 0)
+    label_matrix = label.unsqueeze(1) == label.unsqueeze(0)
+
+    positive_matrix = label_matrix.triu(diagonal=1)
+    negative_matrix = label_matrix.logical_not().triu(diagonal=1)
+
+    similarity_matrix = similarity_matrix.view(-1)
+    positive_matrix = positive_matrix.view(-1)
+    negative_matrix = negative_matrix.view(-1)
+    return similarity_matrix[positive_matrix], similarity_matrix[negative_matrix]
+
+
+class HardCircleLoss(nn.Module):
+    def __init__(self, m: float, gamma: float, alpha: float, beta: float) -> None:
+        super(HardCircleLoss, self).__init__()
+        self.m = m
+        self.gamma = gamma
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, sp: Tensor, sn: Tensor) -> Tensor:
+        ap = torch.clamp_min(- sp.detach() + 1 + self.m, min=0.)
+        an = torch.clamp_min(sn.detach() + self.m, min=0.)
+
+        delta_p = 1 - self.m
+        delta_n = self.m
+
+        logit_p = - ap * (sp - delta_p) * self.gamma
+        logit_n = an * (sn - delta_n) * self.gamma
+
+        loss = torch.log(1 + (torch.sum(torch.exp(logit_n)) ** self.alpha) * (torch.sum(torch.exp(logit_p)) ** self.beta))
+
+        return loss
